7d93954466d4a509bffd1eb6c054b41a6f9b6d85
kangmunju <ikmj0871@gmail.com>
2023-02-16 17:37:17 +0900

    day22mission3

--- src/day22mission.java
+++ src/day22mission.java
@@ -1,26 +1,35 @@
+//3
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class day22mission {
     public static void main(String args[]) {
-        Thread worker = new Thread(()-> {
-            for (int i = 0; i < 5; i++) {
-                System.out.println("작업 스레드 : " + i);
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        Runnable task = () -> {
 
-                }
-            }
-        });
-           worker.start();
+                    try {
+                        for (int i = 0; i < 5; i++) {
+                            System.out.println("작업 스레드 : " + i);
+                            Thread.sleep(1000);}
+                    } catch (InterruptedException e) {
+
+                    }
+                    exec.shutdown();
+
+            };
+
+
+        exec.submit(task);
 
         int alphabet = 'a';
-        try {
-            while (worker.isAlive()) {
-                System.out.println("메인 스레드 : " + (char) alphabet++);
-                Thread.sleep(500);
+
+            try {
+                while(!exec.isShutdown()) {
+                    System.out.println("메인 스레드 : " + (char) alphabet++);
+                    Thread.sleep(500);
+                }
+            } catch (InterruptedException e) {
             }
-        } catch (InterruptedException e) {
-        }
+         }
     }
-}
