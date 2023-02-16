8356daefbcabc61edcdc79608b445c0feb9888cb
kangmunju <ikmj0871@gmail.com>
2023-02-16 16:51:50 +0900

    day22mission2

--- src/day22mission.java
+++ src/day22mission.java
@@ -1,21 +1,20 @@
 
-class Worker extends Thread {
-    public void run() {
-        try {
-            for (int i = 0; i < 5; i++) {
-                System.out.println("작업 스레드 : " + i);
-                Thread.sleep(1000);
-            }
-        } catch (InterruptedException e) {
-        }
-    }
-}
 
 public class day22mission {
     public static void main(String args[]) {
+        Thread worker = new Thread(()-> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("작업 스레드 : " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+
+                }
+            }
+        });
+           worker.start();
+
         int alphabet = 'a';
-        Worker worker = new Worker();
-        worker.start();
         try {
             while (worker.isAlive()) {
                 System.out.println("메인 스레드 : " + (char) alphabet++);
